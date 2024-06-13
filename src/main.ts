7a73cf8e314cb4d525158f166468b91a6d98c6f4
darkkingstar <sounakguha.12345@gmail.com>
2024-06-13 22:57:54 +0530

    Added: swagger UI

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Chopper API')
+    .setDescription('Get ultimate access to the world of anime!')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
